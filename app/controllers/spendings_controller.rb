ec3876d0d481a4a8e30c5b1c92f48ebd3b6856b2
Anna Poltieva <anna.poltieva@kennasecurity.com>
2022-11-02 13:05:08 +0100

    Start working on turbo framse, add spending update

--- app/controllers/spendings_controller.rb
+++ app/controllers/spendings_controller.rb
@@ -1,7 +1,34 @@
 class SpendingsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_spending, only: %i[edit update]
 
   def index
     @spendings = current_user.spendings
+  end
+
+  def new
+    @spending = current_user.spendings.build
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @spending.update(spending_params)
+        format.turbo_stream
+        format.html { redirect_to spendings_path, status: :see_other, notice: "Spending was successfully updated." }
+      else
+        format.html {render :edit}
+      end
+    end
+  end
+
+  private
+  def spending_params
+    params.require(:spending).permit(:name, :amount, :category)
+  end
+
+  def find_spending
+    @spending = current_user.spendings.find(params[:id])
   end
 end
