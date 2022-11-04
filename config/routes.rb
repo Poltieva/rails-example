a5cfe2e5c223a9a51aca61823f5d6fcc8de6f1b1
Anna Poltieva <anna.poltieva@kennasecurity.com>
2022-11-04 14:01:19 +0100

    Add forgetten constraint on spendings routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :spendings
+  resources :spendings, except: :show
   root 'spendings#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
